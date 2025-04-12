3b88cd78a136c176a52dca456d5d8c4e4660f96a
Silviu Bogan <silviubogan@gmail.com>
2025-04-12 03:02:36 +0300

    Set base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  basePath: '/home',
 };
 
 export default nextConfig;
