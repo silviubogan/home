ae68895baeacd29f5e8962a85fc09ae127b3e6b3
Silviu Bogan <silviubogan@gmail.com>
2025-04-17 23:47:35 +0300

    Untested likes page

---
+++ src/app/likes/images.tsx
@@ -0,0 +1,9 @@
+import { MyPhoto } from '@/lib/images';
+
+export const likes: MyPhoto[] = [
+    {
+        src: 'https://i.imgur.com/gioXevX.jpeg',
+        width: 0,
+        height: 0,
+    },
+]
---
+++ src/app/likes/page.tsx
@@ -0,0 +1,6 @@
+import { likes } from "./images";
+import { getImages } from '@/lib/images';
+
+export default async function Page() {
+    return <h1>Likes</h1>
+}
