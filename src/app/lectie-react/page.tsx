e25a73b49668b65bd8f35e6acc0f3ab9ce696c27
Silviu Bogan <silviubogan@gmail.com>
2025-12-18 04:19:07 +0200

    Start React lesson, add donate button

--- src/app/lectie-react/page.tsx
+++ src/app/lectie-react/page.tsx
@@ -1,5 +1,36 @@
+import CodeBlock from '@/components/CodeBlock';
+import Image from 'next/image';
+
 export default function Home() {
   return <article>
-    <h2>Prima lecție React este în lucru.</h2>
+    <h2>Ce este React?</h2>
+    <p><strong>Biblioteca pentru interfețe de utilizator în navigator și native</strong></p>
+    <h3>Creează interfețe de utilizator din componente</h3>
+    <p>
+      React îți permite să construiești interfețe de utilizator din piese individuale numite componente.
+      Creează-ți propriile componente ca <code>Miniatură</code>, <code>ButonÎmiPlace</code> și <code>Video</code>.
+      Apoi combină-le în ecrane, pagini și aplicații întregi.
+    </p>
+    <p><strong>Video.js</strong></p>
+    <CodeBlock block={`function Video({ video }) {
+  return (
+    <div>
+      <Miniatură video={video} />
+      <a href={video.url}>
+        <h3>{video.titlu}</h3>
+        <p>{video.descriere}</p>
+      </a>
+      <ButonÎmiPlace video={video} />
+    </div>
+  );
+}`} />
+    <p><em>Va urma.</em></p>
+
+    <form action="https://www.paypal.com/ncp/payment/D6U2KM8LALZLJ" method="post" target="_blank"
+        style={{ display: "inline-grid", justifyItems: "center", alignContent: "start", gap: "0.5rem" }}>
+      <input className="pp-D6U2KM8LALZLJ" type="submit" value="Cumpără-mi o cafea" />
+      <Image src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg" alt="cards" width={100} height={100} />
+      <section style={{ fontSize: "0.75rem" }}> Prin <img src="https://www.paypalobjects.com/paypal-ui/logos/svg/paypal-wordmark-color.svg" alt="paypal" style={{ height: "0.875rem", verticalAlign: "middle" }} /></section>
+    </form>
   </article>;
 }
