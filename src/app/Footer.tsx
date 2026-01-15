3463ce75b31c9582f1ac4bd280b742204b5470ed
Silviu Bogan <silviubogan@gmail.com>
2026-01-15 06:51:54 +0200

    Better footer, style

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -5,15 +5,37 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="list-footer">
-      <form action="https://www.paypal.com/ncp/payment/D6U2KM8LALZLJ" method="post" target="_blank"
-        style={{ display: "inline-grid", justifyItems: "center", alignContent: "start", gap: "0.5rem", marginTop: "1rem" }}>
-        <input className="pp-D6U2KM8LALZLJ" type="submit" value="Cumpără-mi o cafea" />
-        <Image src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg" alt="cards" width={140} height={20} />
-        <section style={{ fontSize: "0.75rem" }}> Prin <strong>PayPal</strong></section>
+      <form
+        action="https://www.paypal.com/ncp/payment/D6U2KM8LALZLJ"
+        method="post"
+        target="_blank"
+        style={{
+          display: "inline-grid",
+          justifyItems: "center",
+          alignContent: "start",
+          gap: "0.5rem",
+          marginTop: "1rem",
+        }}
+      >
+        <input
+          className="pp-D6U2KM8LALZLJ"
+          type="submit"
+          value="Cumpără-mi o cafea"
+        />
+        <Image
+          src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg"
+          alt="cards"
+          width={140}
+          height={20}
+        />
+        <section style={{ fontSize: "0.75rem" }}>
+          {" "}
+          Prin <strong>PayPal</strong>
+        </section>
       </form>
       <p>
-        Lucrez cu Manjaro Linux / Windows, VS Code, Git, GitHub,
-        React, Next.js, TypeScript, HTML, CSS, Node.js, NPM.
+        Lucrez cu VS Code, Git, GitHub, React, Next.js, TypeScript, HTML, CSS,
+        Node.js, NPM. (În trecut cu foarte multe altele.)
       </p>
       <p>
         Luați legătura cu mine{" "}
@@ -29,6 +51,13 @@ const Footer = () => {
           <GitHubIcon label="GitHub" />
         </a>{" "}
         am unele depozite interesante și contribuții la Volto.
+      </p>
+      <p>
+        Raportați probleme ale site-ului (și dați-mi steluță!){" "}
+        <a href="https://github.com/silviubogan/home/issues/new">
+          <GitHubIcon label="pe GitHub" />
+        </a>
+        .
       </p>
       <p style={{ textAlign: "center" }}>© 2025 Silviu Bogan</p>
     </footer>
