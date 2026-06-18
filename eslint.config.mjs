97e340bf27f14f3bd3694d8c5e39fc185718a380
Silviu Bogan <silviubogan@gmail.com>
2026-06-18 04:33:01 +0300

    Solve linter-detected issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,21 @@
 import js from "@eslint/js";
 import globals from "globals";
-import tseslint from 'typescript-eslint';
+import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 import nextPlugin from "@next/eslint-plugin-next";
 
 export default defineConfig([
+  {
+    ignores: [".next"],
+  },
   js.configs.recommended,
   tseslint.configs.recommended,
   react.configs.flat.recommended,
-  nextPlugin.configs['core-web-vitals'],
+  nextPlugin.configs["core-web-vitals"],
   // nextPlugin.configs.recommended,
   {
-    files: [ "**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}" ],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     languageOptions: { globals: globals.browser },
     rules: {
       "react/react-in-jsx-scope": "off",
@@ -21,9 +24,9 @@ export default defineConfig([
     },
   },
   {
-    "settings": {
-      "react": {
-        "version": "detect", // in future eslint-plugin-react this will be the default
+    settings: {
+      react: {
+        version: "detect", // in future eslint-plugin-react this will be the default
       },
     },
   },
--- src/components/CMatrixScript.tsx
+++ src/components/CMatrixScript.tsx
@@ -3,6 +3,7 @@ import Script from 'next/script';
 
 export const CMatrixScript = function () {
   return (
+    // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
     <Script
       src="https://cdn.jsdelivr.net/npm/cmatrix"
       strategy="beforeInteractive" // intentional
