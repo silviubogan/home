d6d3c2f2e9902d54db518f701a0ca52508112369
Silviu Bogan <silviubogan@gmail.com>
2026-06-18 03:13:50 +0300

    Upgrade deps, correct ESLint config, update tsconfig.json, update MyParticles.tsx
    (there are lots of TS ESLint issues to be fixed)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import tseslint from 'typescript-eslint';
+import react from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  react.configs.flat.recommended,
   {
     files: [ "**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}" ],
-    plugins: { js },
-    extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
+    rules: {
+      // your overrides here
+    },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ]);
--- src/components/MyParticles.tsx
+++ src/components/MyParticles.tsx
@@ -1,117 +1,107 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import type { /* Container, */ Engine } from "@tsparticles/engine";
+import React, { useMemo, useState } from "react";
+import Particles, { ParticlesProvider } from "@tsparticles/react";
+import { type Container, type Engine, type ISourceOptions, MoveDirection, OutMode } from "@tsparticles/engine";
 // import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
-export const MyParticles: React.FC = () => {
-  const [init, setInit] = useState(false);
+// TODO: use https://github.com/tsparticles/tsparticles/tree/main/wrappers/nextjs#readme
 
-  // this should be run only once per application lifetime
-  useEffect(() => {
-    initParticlesEngine(async (engine: Engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
-      await loadSlim(engine);
-      //await loadBasic(engine);
-    }).then(() => {
-      setInit(true);
-    });
+// this should be run only once per application lifetime (?)
+const particlesInit = async (engine: Engine) => {
+  await loadSlim(engine);
+};
+
+export const MyParticles: React.FC = () => {
+  const options: ISourceOptions = useMemo(() => {
+    return {
+      background: {
+        color: {
+          value: "white",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: false,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+          resize: {
+            delay: 0,
+            enable: true,
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#dddddd",
+        },
+        links: {
+          color: "#dddddd",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            // area: {
+            //   width: 800,
+            //   height: 800,
+            //   enable: true,
+            // },
+          },
+          value: 100,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    };
   }, []);
 
-  // const particlesLoaded = async (container?: Container | undefined) => {
-  //   console.log(container);
-  // };
-
   return (
-    init && (
+    <ParticlesProvider init={particlesInit}>
       <Particles
         id="tsparticles"
         // particlesLoaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: "white",
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: false,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: {
-                delay: 0,
-                enable: true,
-              },
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#dddddd",
-            },
-            links: {
-              color: "#dddddd",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                // area: {
-                //   width: 800,
-                //   height: 800,
-                //   enable: true,
-                // },
-              },
-              value: 100,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
+        options={options}
       />
-    )
+    </ParticlesProvider>
   );
 };
