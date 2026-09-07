d02df5f9c2bd6a37764f6c853d055b7bd56aaa93
Silviu Bogan <silviubogan@gmail.com>
2026-09-08 02:05:31 +0300

    Style Patreon link, run Prettier on some code

--- src/app/MyHome.tsx
+++ src/app/MyHome.tsx
@@ -22,7 +22,8 @@ export function MyHome({ images }: { images: MyImages }) {
     <main>
       <p>
         Lucrez cu VS Code, Git, GitHub, React, Next.js, TypeScript, HTML, CSS,
-        Node.js, P/NPM, Volto. <small>(În trecut cu foarte multe altele.)</small>
+        Node.js, P/NPM, Volto.{" "}
+        <small>(În trecut cu foarte multe altele.)</small>
       </p>
       <p>
         Pe{" "}
@@ -31,24 +32,26 @@ export function MyHome({ images }: { images: MyImages }) {
         </a>{" "}
         am unele depozite interesante și contribuții.
       </p>
-      <p>
-        Clic aici pentru a mă susține pe Patreon:
-        <a href="https://patreon.com/SilviuBogan?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink">
-          <Image
-            src="https://i.imgur.com/hL5qxJN.png"
-            alt="Imagine cu link către pagina mea Patreon"
-            width={1200 / 3.5}
-            height={630 / 3.5}
-            style={{
-              display: "block",
-              marginLeft: "auto",
-              marginRight: "auto",
-              height: "auto",
-              maxWidth: "100%",
-            }}
-          />
-        </a>
-      </p>
+      <section>
+        <p>Clic aici pentru a mă susține pe Patreon:</p>
+        <div className="patreon-link">
+          <a href="https://patreon.com/SilviuBogan?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink">
+            <Image
+              src="https://i.imgur.com/hL5qxJN.png"
+              alt="Imagine cu link către pagina mea Patreon"
+              width={1200 / 3.5}
+              height={630 / 3.5}
+              style={{
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                height: "auto",
+                maxWidth: "100%",
+              }}
+            />
+          </a>
+        </div>
+      </section>
       <p>Portofoliu parțial:</p>
       <Accordion
         headers={[
@@ -227,9 +230,7 @@ export function MyHome({ images }: { images: MyImages }) {
               <WikipediaIcon label="Utilizator:Silviubogan" />
             </a>
           </p>,
-          <p key={120}>
-            Primele 2 firme au fost: jillix, EDW.
-          </p>,
+          <p key={120}>Primele 2 firme au fost: jillix, EDW.</p>,
         ]}
       />
     </main>
